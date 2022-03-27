75210549c25574563385631063ef1ac4133054a4
minhchau <minhchaunguyenmtp@gmail.com>
2022-03-27 12:37:52 +0700

    relace button onclick fragment

--- app/src/main/java/com/example/android_week_06/MainActivity.java
+++ app/src/main/java/com/example/android_week_06/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android_week_06;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 private Button btn_01, btn_02;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,34 @@ private Button btn_01, btn_02;
         btn_02=findViewById(R.id.btn_Frag_02);
 
 
+        Fragment Fragment_01 = new Fragment_01();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
+        transaction.replace(R.id.flfragment,Fragment_01);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+        btn_01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment Fragment_01 = new Fragment_01();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                transaction.replace(R.id.flfragment,Fragment_01);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+        btn_02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment Fragment_02 = new Fragment_02();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                transaction.replace(R.id.flfragment,Fragment_02);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
     }
 }
